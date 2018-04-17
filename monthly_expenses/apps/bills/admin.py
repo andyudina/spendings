08c42b13ca29340224df7c856ccb605f9a7bf187
Anastasiia Diudina <anastasiia@holvi.com>
2018-04-17 13:51:15 +0300

    Add custom models to django admin

--- monthly_expenses/apps/bills/admin.py
+++ monthly_expenses/apps/bills/admin.py
@@ -1,6 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import json
 
 from django.contrib import admin
+from django.utils.html import format_html_join
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from apps.spendings.models import Spending
+from apps.bills.models import Bill
+
+
+class SpendingInline(admin.TabularInline):
+    model = Spending
+
+
+class BillAdmin(admin.ModelAdmin):
+    inlines = (SpendingInline, )
+    readonly_fields = ('parsed_data', )
+
+    def parsed_data(self, bill):
+        # print json of parsed bill
+        # or error if bill can niot be parsed
+        try:
+            data = bill.parse_bill()
+            return format_html_join(
+               mark_safe('<pre>'),
+               json.dumps(data),
+               mark_safe('</pre>'),
+            )
+        except ValueError as e:
+            error = e.args[0]
+            return format_html_join(
+                mark_safe('<span class="errors">'),
+                error,
+                mark_safe('<span>'),
+            )
+
+
+admin.site.register(Bill, BillAdmin)
