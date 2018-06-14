a79527772ce7af52cd1e2fc5d78448223f15d0b0
Anastasia <anastasia.diudina@gmail.com>
2018-06-14 17:22:15 +0300

    Create budgeting category in admin

--- monthly_expenses/apps/budgets/__init__.py
+++ monthly_expenses/apps/budgets/__init__.py
@@ -2,3 +2,4 @@
 App for budgets and their categories.
 Responsible for creating and monitoring budgets
 """
+default_app_config = 'apps.budgets.django_apps.BudgetsConfig'
--- monthly_expenses/apps/budgets/models.py
+++ monthly_expenses/apps/budgets/models.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+from django.db import models
+
+
+class Category(models.Model):
+    """
+    Store budget period category
+    """
+    name = models.CharField(
+        verbose_name='Name',
+        max_length=255)
+
+    def __str__(self):
+        return self.name
